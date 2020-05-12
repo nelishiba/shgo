9ea512efc9ee8242091014b75f7926f4e35cf114
Hiroki Watanabe <nabehiro@keio.jp>
2020-05-12 13:13:52 +0900

    add ast interface

---
+++ ast/ast.go
@@ -0,0 +1,45 @@
+package ast
+
+import "github.com/negli0/shgo/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type CmdStatement struct {
+	Token token.Token
+	Name  *Identifier
+}
+
+func (cs *CmdStatement) statementNode()        {}
+func (cs *CmdStatement) TookenLiteral() string { return cs.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
